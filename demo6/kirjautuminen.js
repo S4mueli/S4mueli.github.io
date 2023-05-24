ed36ec67b11dc5e7221f322f30afae2da809d04c
S4mueli <132884355+S4mueli@users.noreply.github.com>
2023-05-24 09:51:03 +0300

    3

---
+++ demo6/kirjautuminen.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",inkoKirjautunut);
+function onkoKirjautunut(){
+    let kirjautunut = 'kylla';
+    if(kirjautunut === 'kylla'){
+document.getElementById('tervetulo_teksti').textContent += 'Samueli!'; 
+}
+}
